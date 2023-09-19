1eb98bf7190dfb81b64f9fce778f17c3631d071c
david chivato de la fuente <105808314+destroyer-dcf@users.noreply.github.com>
2023-09-19 13:38:50 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+from gotocpc import __version__ as version
+
+VERSION = version
+DESCRIPTION = 'Software Developer Kit for programming in Basic for Amstrad CPC'
+
+setup(
+    name='gotocpc',
+    version=VERSION,
+    author="Destroyer",
+    author_email="<destroyer.dcf@gmail.com>",
+    description=DESCRIPTION,
+    license="GPL",
+    packages=find_packages(),
+    data_files=[
+        ('gotocpc/bin/linux', ['gotocpc/bin/linux/iDSK', 'gotocpc/bin/linux/martine']),
+        ('gotocpc/bin/darwin',['gotocpc/bin/darwin/iDSK', 'gotocpc/bin/darwin/martine']),
+        ('gotocpc/bin/win',   ['gotocpc/bin/win/iDSK.exe', 'gotocpc/bin/win/martine.exe']),
+        ('gotocpc/bin',       ['gotocpc/bin/ccz80.exe', 'gotocpc/bin/ccz80.exe']),
+        ('gotocpc/bin/win',   ['gotocpc/bin/win/cyggcc_s-1.dll']),
+        ('gotocpc/bin/win',   ['gotocpc/bin/win/cygwin1.dll']),
+        ('gotocpc/templates', ['gotocpc/templates/cpc.j2']),
+        ('gotocpc/includes',  ['gotocpc/includes/cpc464.ccz80']),
+        ('gotocpc/includes',  ['gotocpc/includes/cpc6128.ccz80']),
+        ('gotocpc/includes',  ['gotocpc/includes/CPMPlus.ccz80']),
+        ('gotocpc/includes',  ['gotocpc/includes/SpritesAlive.ccz80']),
+        ('gotocpc/includes',  ['gotocpc/includes/sprUtilCPC.ccz80']),
+        ('gotocpc/includes',  ['gotocpc/includes/standard.ccz80'])
+    ],
+    install_requires=[
+        'click',
+        'configparser',
+        'rich',
+        'PyYAML',
+        'jinja2',
+        'emoji',
+        'jsonschema',
+        'python-dotenv'
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License (GPL)',   
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: POSIX :: Linux',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gotocpc=gotocpc.__main__:main',
+            'go2cpc=gotocpc.__main__:main',
+            'cpc=gotocpc.__main__:main'
+        ]
+    }
+)
