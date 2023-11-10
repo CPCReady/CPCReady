7447c5b3102e831f7098eb8dbe6ffef5a8f9b90f
destroyer <destroyer.dcf@gmail.com>
2023-11-10 11:12:13 +0000

    refactor only linux and add binarys and z88dk

--- CPCReady/common.py
+++ CPCReady/common.py
@@ -1,115 +1,113 @@
-from ctypes.wintypes import RGB
-import os
+
+
 import sys
-import datetime
-import time
 import logging
 import shutil
 import configparser
-from rich.console import Console
+import platform
 from rich.logging import RichHandler
-from rich.text import Text
 from rich.console import Console
-from rich import inspect
 from rich.table import Table
 from rich import print
-from rich.columns import Columns
-from configparser import ConfigParser
-import configparser as cfg
 from jinja2 import Template
 import ipaddress as ip
-import platform
-import subprocess
-import requests
 import os
-
 
 console = Console()
 log = logging.getLogger("rich")
 
 FORMAT = "%(message)s"
 logging.basicConfig(
-    level="NOTSET",format=FORMAT, datefmt="[%X]", handlers=[RichHandler(rich_tracebacks=False, show_path=False,omit_repeated_times=True)]
+    level="NOTSET", format=FORMAT, datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=False, show_path=False, omit_repeated_times=True)]
 )
-
 
 ## Common Variables
 #
 ##
 
-subfolders        = ["out", "out/disc", "src", "cfg","lib","img","spr","docs"]
-PWD               = os.getcwd() + "/"
-TEMPLATE_RVM_WEB  = "rvm-web.html"
-PATH_CFG          = "cfg"
-PATH_DISC         = "out/disc"
-PATH_OBJ          = "obj"
-PATH_SRC          = "src"
-PATH_DSK          = "out"
-PATH_LIB          = "lib"
-PATH_SPR          = "spr"
-PATH_ASSETS       = "img"
-CFG_PROJECT       = f"{PATH_CFG}/project.cfg"
-CFG_EMULATORS     = f"{PATH_CFG}/emulators.cfg"
-CFG_IMAGES        = f"{PATH_CFG}/images.cfg"
-CFG_SPRITES       = f"{PATH_CFG}/sprites.cfg"
-APP_PATH          = os.getenv('CPCREADY')
-SECTIONS_PROJECT  = ["general", "configurations", "CDT", "DSK"]
-SECTIONS_EMULATOR = ["rvm-web", "rvm-desktop","m4board"]
-EMULATORS_TYPES   = ["web", "desktop","m4board"]
-CPC_MODELS        = ["6128","464","664"]
+subfolders = ["out", "out/disc", "src", "cfg", "lib", "img", "spr", "docs"]
+PWD = os.getcwd() + "/"
+TEMPLATE_RVM_WEB = "rvm-web.html"
+PATH_CFG = "cfg"
+PATH_DISC = "out/disc"
+PATH_OBJ = "obj"
+PATH_SRC = "src"
+PATH_DSK = "out"
+PATH_LIB = "lib"
+PATH_SPR = "spr"
+PATH_ASSETS = "img"
+CFG_PROJECT = f"{PATH_CFG}/project.cfg"
+CFG_EMULATORS = f"{PATH_CFG}/emulators.cfg"
+CFG_IMAGES = f"{PATH_CFG}/images.cfg"
+CFG_SPRITES = f"{PATH_CFG}/sprites.cfg"
+APP_PATH = os.getenv('CPCREADY')
+SECTIONS_PROJECT = ["general", "configurations", "CDT", "DSK"]
+SECTIONS_EMULATOR = ["rvm-web", "rvm-desktop", "m4board"]
+EMULATORS_TYPES = ["web", "desktop", "m4board"]
+CPC_MODELS = ["6128", "464", "664"]
 
 if sys.platform.startswith('linux'):
     TEMP_PATH = os.getenv('HOME') + "/tmp"
-    MARTINE   = os.getenv('CPCREADY') + "/tools/bin/martine"
-    IDSK      = os.getenv('CPCREADY') + "/tools/bin/iDSK"
-    UGBASIC   = os.getenv('CPCREADY') + "/tools/bin/ugbc"
-    AMSDOS    = os.getenv('CPCREADY') + "/tools/bin/amsdos"
-    CDT       = os.getenv('CPCREADY') + "/tools/bin/2cdt"
-    CPC2CDT   = os.getenv('CPCREADY') + "/tools/bin/cpc2cdt"
-    M4BOARD   = os.getenv('CPCREADY') + "/tools/bin/xfer"
+    MARTINE = os.path.dirname(os.path.abspath(__file__)) + "/binary/martine"
+    IDSK = os.path.dirname(os.path.abspath(__file__)) + "/binary/iDSK"
+    UGBASIC = os.path.dirname(os.path.abspath(__file__)) + "/binary/ugbc"
+    AMSDOS = os.path.dirname(os.path.abspath(__file__)) + "/binary/amsdos"
+    CDT = os.path.dirname(os.path.abspath(__file__)) + "/binary/2cdt"
+    CPC2CDT = os.path.dirname(os.path.abspath(__file__)) + "/binary/cpc2cdt"
+    M4BOARD = os.path.dirname(os.path.abspath(__file__)) + "/binary/xfer"
+    RETROVIRTUALMACHINE = os.path.dirname(os.path.abspath(__file__)) + "/binary/RetroVirtualMachine"
+
+TEMPLATES_PATH = os.path.dirname(os.path.abspath(__file__)) + "/cfg/"
 
 CONVERSION_PALETTE = {
-    "COLOR_0":"RGB(0,0,0)",
-    "COLOR_1":"RGB(0,0,128)",
-    "COLOR_2":"RGB(0,0,255)",
-    "COLOR_3":"RGB(128,0,0)",
-    "COLOR_4":"RGB(128,0,128)",
-    "COLOR_5":"RGB(128,0,255)",
-    "COLOR_6":"RGB(255,0,0)",
-    "COLOR_7":"RGB(255,0,128)",
-    "COLOR_8":"RGB(255,0,255)",
-    "COLOR_9":"RGB(0,128,0)",
-    "COLOR_00":"RGB(0,0,0)",
-    "COLOR_01":"RGB(0,0,128)",
-    "COLOR_02":"RGB(0,0,255)",
-    "COLOR_03":"RGB(128,0,0)",
-    "COLOR_04":"RGB(128,0,128)",
-    "COLOR_05":"RGB(128,0,255)",
-    "COLOR_06":"RGB(255,0,0)",
-    "COLOR_07":"RGB(255,0,128)",
-    "COLOR_08":"RGB(255,0,255)",
-    "COLOR_09":"RGB(0,128,0)",
-    "COLOR_10":"RGB(0,128,128)",
-    "COLOR_11":"RGB(0,128,255)",
-    "COLOR_12":"RGB(128,128,0)",
-    "COLOR_13":"RGB(128,128,128)",
-    "COLOR_14":"RGB(128,128,255)",
-    "COLOR_15":"RGB(255,128,0)",
-    "COLOR_16":"RGB(255,128,128)",
-    "COLOR_17":"RGB(255,128,255)",
-    "COLOR_18":"RGB(0,255,0)",
-    "COLOR_19":"RGB(0,255,128)",
-    "COLOR_20":"RGB(0,255,255)",
-    "COLOR_21":"RGB(128,255,0)",
-    "COLOR_22":"RGB(128,255,128)",
-    "COLOR_23":"RGB(128,255,255)",
-    "COLOR_24":"RGB(255,255,0)",
-    "COLOR_25":"RGB(255,255,128)",
-    "COLOR_26":"RGB(255,255,255)"
+    "COLOR_0": "RGB(0,0,0)",
+    "COLOR_1": "RGB(0,0,128)",
+    "COLOR_2": "RGB(0,0,255)",
+    "COLOR_3": "RGB(128,0,0)",
+    "COLOR_4": "RGB(128,0,128)",
+    "COLOR_5": "RGB(128,0,255)",
+    "COLOR_6": "RGB(255,0,0)",
+    "COLOR_7": "RGB(255,0,128)",
+    "COLOR_8": "RGB(255,0,255)",
+    "COLOR_9": "RGB(0,128,0)",
+    "COLOR_00": "RGB(0,0,0)",
+    "COLOR_01": "RGB(0,0,128)",
+    "COLOR_02": "RGB(0,0,255)",
+    "COLOR_03": "RGB(128,0,0)",
+    "COLOR_04": "RGB(128,0,128)",
+    "COLOR_05": "RGB(128,0,255)",
+    "COLOR_06": "RGB(255,0,0)",
+    "COLOR_07": "RGB(255,0,128)",
+    "COLOR_08": "RGB(255,0,255)",
+    "COLOR_09": "RGB(0,128,0)",
+    "COLOR_10": "RGB(0,128,128)",
+    "COLOR_11": "RGB(0,128,255)",
+    "COLOR_12": "RGB(128,128,0)",
+    "COLOR_13": "RGB(128,128,128)",
+    "COLOR_14": "RGB(128,128,255)",
+    "COLOR_15": "RGB(255,128,0)",
+    "COLOR_16": "RGB(255,128,128)",
+    "COLOR_17": "RGB(255,128,255)",
+    "COLOR_18": "RGB(0,255,0)",
+    "COLOR_19": "RGB(0,255,128)",
+    "COLOR_20": "RGB(0,255,255)",
+    "COLOR_21": "RGB(128,255,0)",
+    "COLOR_22": "RGB(128,255,128)",
+    "COLOR_23": "RGB(128,255,255)",
+    "COLOR_24": "RGB(255,255,0)",
+    "COLOR_25": "RGB(255,255,128)",
+    "COLOR_26": "RGB(255,255,255)"
 }
 
-
-
+##
+# verificamos si es linux
+def verificar_linux():
+    sistema_operativo = platform.system()
+    if sistema_operativo != 'Linux':
+        mensaje_error = f"\nThis operating system is not supported. Linux only."
+        raise OSError(mensaje_error)
+    
 ##
 # create template file
 #
@@ -118,9 +116,7 @@ CONVERSION_PALETTE = {
 # @param out: generate template directory
 ##
 def createTemplate(templateName, templateData, out):
-    
-    APP_PATH = os.getenv('CPCREADY')
-    with open(APP_PATH + f"/cfg/{templateName}.j2", 'r') as file:
+    with open(TEMPLATES_PATH + f"{templateName}.j2", 'r') as file:
         template_string = file.read()
     template = Template(template_string)
     rendered_template = template.render(templateData)
@@ -139,6 +135,7 @@ def rmFolder(directory):
     if os.path.exists(directory) and os.path.isdir(directory):
         shutil.rmtree(directory)
 
+
 ##
 # get data ini/cfg file
 #
@@ -148,6 +145,7 @@ def getData(cfgFile):
     config = configparser.ConfigParser()
     config.read(cfgFile)
     return config
+
 
 ##
 # Print message warning
@@ -178,8 +176,9 @@ def msgError(message):
 # @param message: message to display
 ##
 def msgInfo(message):
-    #log.info(message, extra={"highlighter": None})
+    # log.info(message, extra={"highlighter": None})
     console.print("[bold blue]  INFO    [/][white]" + message + "[/]")
+
 
 ##
 # Print message custom
@@ -194,22 +193,36 @@ def msgCustom(flavor, message, color):
     if color.upper() == "GREEN":
         console.print(f"[bold green]  {flavor}[/][white]{message}[/]")
     elif color.upper() == "RED":
-        console.print(f"[bold red]  {flavor}[/][bold white]{message}[/]")    
+        console.print(f"[bold red]  {flavor}[/][bold white]{message}[/]")
     elif color.upper() == "BLUE":
-        console.print(f"[bold blue]  {flavor}[/][bold white]{message}[/]")    
+        console.print(f"[bold blue]  {flavor}[/][bold white]{message}[/]")
     elif color.upper() == "YELLOW":
         console.print(f"[bold yellow]  {flavor}[/][bold white]{message}[/]")
 
-def endTask (message,flavor):
+
+def endTask(message, flavor):
+    global MSG
     print()
     if flavor == "OK":
-        console.print(f"🚀  Successfully {message}.")
+        MSG = f"🚀  Successfully {message}."
     if flavor == "ERROR":
-        console.print(f"💥  [red]Unsuccessful {message}.[/]")
+        MSG = f"💥  [red]Unsuccessful {message}.[/]"
+
+    BANNER = Table(show_header=False)
+    TEXT = MSG.ljust(122, " ")
+    BANNER.add_row(TEXT)
+    console.print(BANNER)
+    print()
+
 
 def showInfoTask(message):
-    print()    
-    console.print(f"👉  {message}🍺")
+    BANNER = Table(show_header=False)
+    print()
+    MSG = f"👉  {message}🍺"
+    print(MSG)
+    # TEXT = MSG.ljust(112, " ")
+    # BANNER.add_row(TEXT)
+    # console.print(BANNER)
     print()
 
 
@@ -243,6 +256,7 @@ def getFileExtension(source):
     file_extension = os.path.splitext(source)[1]
     return file_extension
 
+
 ##
 # Show head data proyect
 #
@@ -267,16 +281,20 @@ def showHeadDataProject(project):
 ##
 
 def showFoodDataProject(description, out):
-    description = f"*** {description} ***"
-    center_text = description.center(80)
-    console.print(
-        "[bold yellow]\n==================================================================================== [/bold yellow]")
+    print()
+    BANNER = Table(show_header=False)
     if out == 0:
-        console.print("[bold green]" + center_text.upper() + "[/bold green]")
-        console.print("[bold yellow]====================================================================================\n [/bold yellow]")
+        TEXT = f"🚀  " + description.ljust(110, " ")
+        print(TEXT)
+        # TEXT = "[green]" + TEXT + "[/green]"
+        # BANNER.add_row(TEXT)
+        # console.print(BANNER)
     if out == 1:
-        console.print("[bold red]" + center_text.upper() + "[/bold red]")
-        console.print("[bold yellow]====================================================================================\n [/bold yellow]")
+        TEXT = f"💥  " + description.ljust(110, " ")
+        print(TEXT)
+        # TEXT = "[bold red]" + TEXT + "[/bold red]"
+        # BANNER.add_row(TEXT)
+        # console.print(BANNER)
         sys.exit(1)
 
 
@@ -297,16 +315,16 @@ def fileExist(source):
 #
 # @param directory: directory name
 ##
-def removeContentDirectory (directory):
-
+def removeContentDirectory(directory):
     if os.path.exists(directory) and os.path.isdir(directory):
         archivos = os.listdir(directory)
         for archivo in archivos:
             ruta_completa = os.path.join(directory, archivo)
             if os.path.isfile(ruta_completa):
                 os.remove(ruta_completa)
-    msgInfo(f"Clean temporal directory.")            
-                
+    msgCustom("DELETE", "Temporal directory.", "green")
+
+
 ##
 # compilation image
 #
@@ -319,6 +337,7 @@ def imageCompilation(image):
     console.print(
         "[bold white]------------------------------------------------------------------------------------- [/bold white]\n")
 
+
 def readProjectIni(file):
     config = configparser.ConfigParser()
     config.read(file)
@@ -329,8 +348,8 @@ def readProjectIni(file):
             diccionario[seccion][clave] = valor
     return diccionario
 
-def crear_entrada_ini(ruta_archivo, seccion, clave, valor):
 
+def create_entry_ini(ruta_archivo, seccion, clave, valor):
     config = configparser.ConfigParser()
     config.read(ruta_archivo)
     if seccion not in config.sections():
@@ -349,33 +368,33 @@ def validateSection(file, sectionfile):
             return True
     return False
 
-def validate_cfg(cfg,datos):
+
+def validate_cfg(cfg, datos):
     if not fileExist(cfg):
-        sys.exit(1)    
+        sys.exit(1)
     for section in datos:
-        if not validateSection(cfg,section):
-            msgError(f"{section} configuration does not exist in the {cfg} file") 
+        if not validateSection(cfg, section):
+            msgError(f"{section} configuration does not exist in the {cfg} file")
             sys.exit(1)
 
 
-def validateCPCModel(model): 
+def validateCPCModel(model):
     for modelos in CPC_MODELS:
         if str(modelos) == str(model):
             return True
     return False
-    
+
 
 def validateIP(ip_string):
-   try:
-       ip_object = ip.ip_address(ip_string)
-       msgInfo(f"IP address ==> {ip_string}") 
-       return True
-   except ValueError:
-       msgError("IP address ==> '{ip_string}' is not valid")
-       return False       
+    try:
+        ip_object = ip.ip_address(ip_string)
+        msgInfo(f"IP address ==> {ip_string}")
+        return True
+    except ValueError:
+        msgError("IP address ==> '{ip_string}' is not valid")
+        return False
 
-
-# def ping(host):
+    # def ping(host):
 #     param = '-n' if platform.system().lower()=='windows' else '-c'
 #     command = ['ping', param, '1', host]
 #     return subprocess.call(command) == 0
--- CPCReady/func_info.py
+++ CPCReady/func_info.py
@@ -8,63 +8,75 @@ from rich.table import Table
 from rich import print
 from rich.columns import Columns
 from CPCReady import common as cm
+from CPCReady import func_update as update
+
 import sys
 import os
 import random
 from CPCReady import __version__ as version
+
 console = Console()
 
 
-     
 ##
 # Show banner dependencie model cpc
-#@
+# @
 # param cpc: Model CPC
 ##
-def show():
+def show(description=True):
     print()
-            
-    cpc = random.choice(cm.CPC_MODELS)
-    if cpc == "6128":
-        lineSize = 93
-    elif cpc == "464":
-        lineSize = 75
-    elif cpc == "664":
-        lineSize = 75
-    
-    if os.getenv("ENVIRONMENT") == "USER":
-        Linea3 = f"Development Environment for User".ljust(lineSize, " ")
+
+    #     cpc = random.choice(cm.CPC_MODELS)
+    #     if cpc == "6128":
+    #         lineSize = 93
+    #     elif cpc == "464":
+    #         lineSize = 75
+    #     elif cpc == "664":
+    #         lineSize = 75
+
+    #     Linea3 = description.ljust(lineSize - 1, " ")
+    #     Linea1 = f"CPCReady v{version}".ljust(lineSize, " ")
+    #     Linea2 = f"👉 https://cpcready.github.io/doc/".ljust(lineSize - 1, " ")
+
+    #     CPC464 = f"""[bold white]{Linea1}[/]╔═╗╔═╗╔═╗ ┏┓┏┓┏┓ ┌─────────────┐  ON 🟢
+    # [bold white]{Linea2}[/]║  ╠═╝║   ┃┃┣┓┃┃ │[red] ███ [green]███ [blue]███ [white]│
+    # [bold white]{Linea3}[/]╚═╝╩  ╚═╝ ┗╋┗┛┗╋ └─────────────┘ COLOR"""
+
+    #     CPC664 = f"""[bold white]{Linea1}[/]╔═╗╔═╗╔═╗ ┏┓┏┓┏┓ ┌─────────────┐  ON 🟢
+    # [bold white]{Linea2}[/]║  ╠═╝║   ┣┓┣┓┃┃ │[red] ███ [green]███ [blue]███ [white]│
+    # [bold white]{Linea3}[/]╚═╝╩  ╚═╝ ┗┛┗┛┗╋ └─────────────┘ COLOR"""
+
+    #     CPC6128 = f"""[bold white]{Linea1}[/]┌─────────────┐  ENC.
+    # [bold white]{Linea2}[/]│[red] ███ [green]███ [blue]███ [white]│  [green]▄▄▄[/green]
+    # [bold white]{Linea3}[/]└─────────────┘"""
+    check_version_local = update.check_version()
+    if not check_version_local == "99.99.99":
+        new_version = f"👋 New version {check_version_local} found. Please Upgrade.!!![/]"
     else:
-        Linea3 = f"Solution Development Environment".ljust(lineSize, " ")
-    Linea1 = f"CPCReady v{version}".ljust(lineSize, " ")
-    Linea2 = f"CLI Software Developer Kit for programming Amstrad Basic/Ugbasic".ljust(lineSize, " ")
-    
+        new_version = ""
 
-    CPC464 = f"""[bold white]{Linea1}[/]╔═╗╔═╗╔═╗ ┏┓┏┓┏┓ ┌─────────────┐  ON 🟢
-[bold white]{Linea2}[/]║  ╠═╝║   ┃┃┣┓┃┃ │[red] ███ [green]███ [blue]███ [white]│
-[bold white]{Linea3}[/]╚═╝╩  ╚═╝ ┗╋┗┛┗╋ └─────────────┘ COLOR"""
+    LOGOCPCREADY = f"""[bold white]╔═╗╔═╗╔═╗ ┌─────────────┐                                                            
+[bold white]║  ╠═╝║   │[red] ███ [green]███ [blue]███ [white]│[bold white]                 {new_version}[/]
+[bold white]╚═╝╩  ╚═╝ └─────────────┘
+[bold yellow]Ready[/]
+[bold yellow]█[/]                                                                                                   [bold green]v{version}"""
 
-    CPC664 = f"""[bold white]{Linea1}[/]╔═╗╔═╗╔═╗ ┏┓┏┓┏┓ ┌─────────────┐  ON 🟢
-[bold white]{Linea2}[/]║  ╠═╝║   ┣┓┣┓┃┃ │[red] ███ [green]███ [blue]███ [white]│
-[bold white]{Linea3}[/]╚═╝╩  ╚═╝ ┗┛┗┛┗╋ └─────────────┘ COLOR"""
-
-
-    CPC6128 = f"""[bold white]{Linea1}[/]┌─────────────┐  ENC.
-[bold white]{Linea2}[/]│[red] ███ [green]███ [blue]███ [white]│  [green]▄▄▄[/green]
-[bold white]{Linea3}[/]└─────────────┘"""
-    
-    
     BANNER = Table(show_header=False)
 
-    if cpc == "6128":
-        BANNER.add_row(CPC6128)
-    elif cpc == "464":
-        BANNER.add_row(CPC464)
-    elif cpc == "664":
-        BANNER.add_row(CPC664)
-    else:
-        cm.msgError("Model CPC not supported")
-        sys.exit(1)
+    # if cpc == "6128":
+    #     BANNER.add_row(CPC6128)
+    # elif cpc == "464":
+    #     BANNER.add_row(CPC464)
+    # elif cpc == "664":
+    #     BANNER.add_row(CPC664)
+    # else:
+    #     cm.msgError("Model CPC not supported")
+    #     sys.exit(1)
 
+    BANNER.add_row(LOGOCPCREADY)
     console.print(BANNER)
-    
+
+    if description:
+        print()
+        print("[bold white]Github: [/]https://github.com/CPCReady/installer")
+        print("[bold white]Docs  : [/]https://cpcready.github.io/doc/")
